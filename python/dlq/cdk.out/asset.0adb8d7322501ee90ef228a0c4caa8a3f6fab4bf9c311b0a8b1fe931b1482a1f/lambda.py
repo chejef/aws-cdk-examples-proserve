3e669159c8e729fa6790c6b75bc8ec789b9f3ed6
Jeff Chen <91078682+chejef@users.noreply.github.com>
2022-02-23 11:33:23 -0500

    Add files via upload

---
+++ python/dlq/cdk.out/asset.0adb8d7322501ee90ef228a0c4caa8a3f6fab4bf9c311b0a8b1fe931b1482a1f/lambda.py
@@ -0,0 +1,16 @@
+import logging
+
+logger = logging.getLogger()
+logger.setLevel("INFO")
+
+
+def handler(event: dict, context):
+    """
+    The handler for enrichment process.
+
+    Args:
+        event (dict): contains the data received from its data source.
+        context: contains the lambda invocation context.
+    """
+    logger.info(f"event - {event}")
+    raise ValueError("Fail the Lambda function.")
